ab2e9eab7c38ef47e8c04a50ee93138241941aae
Silvio Rodrigues Vieira <srodrvie@everis.com>
2020-10-25 10:15:03 -0300

    first commit

---
+++ src/Exercicio/Exercicio.java
@@ -0,0 +1,29 @@
+package Exercicio;
+
+import java.util.Scanner;
+
+public class Exercicio {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+	
+	System.out.println("Escreva o nº de meses");   
+	
+	int n = s.nextInt();
+	
+	int mes = 30;
+	
+			
+	int calculo = mes * n;
+	
+	
+
+	System.out.println("o valor dia é "+ calculo);
+	
+	
+}
+}
+	
+
+		
